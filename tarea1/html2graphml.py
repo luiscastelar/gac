f74e67024653ff9299f2af2234caf83ad0960fc3
Luis Ferreira <luisferreira@iescastelar.com>
2025-12-11 21:24:11 +0100

    GraphML completo pendiente de test

--- tarea1/html2graphml.py
+++ tarea1/html2graphml.py
@@ -19,7 +19,8 @@ ANCHO = 120                 # Ancho de pantalla
 # Variables globales
 #root = None
 #actual = None
-
+numNodo = 0
+numArista = 0
 
 
 # Main()
@@ -44,15 +45,33 @@ def main():
             print(f'Grafo:\n{grafo.generate(dom)}')
             pass
         case 2: # graphml
-            TEMPLATE = './templates/graphml.xml'
+            TEMPLATE = './gac/tarea1/templates/graphml.xml'
             claves = getTipoDeAtributos(dom)
-            print(claves)
-            #nodos, aristas = getNodosYAristas(dom)
-            #salida = reemplazar(TEMPLATE, 'claves', claves)
-            #salida = reemplazar(salida, 'nodos', nodos)
-            #salida = reemplazar(salida, 'aristas', aristas)
+            clavesNominadas = {}
+            numeral = 0
+            for clave in claves:
+                clavesNominadas[clave] = f'd{numeral}'
+                numeral += 1
+            #print(f'Claves: {clavesNominadas}')
+            claves = ''
+            for nombre, id in clavesNominadas.items():
+                claves += f'\t<key id="{id}" for="node" attr.name="{nombre}" attr.type="string" />\n'
+            print(f'Claves:\n {claves}')
+
+            listaDeNodos, listaDeAristas = getNodosYAristas(dom, clavesNominadas)
+            #print(f'Nodos: {nodos} y \nAristas: {aristas}')
+            nodos = '\n'.join(listaDeNodos)
+            print(f'Nodos:\n{nodos}')
+            aristas = '\n'.join(listaDeAristas)
+            print(f'Aristas:\n{aristas}')
             
-            pass
+            salida = File().load(TEMPLATE)
+            salida = salida.replace("<%claves%>", claves)
+            salida = salida.replace("<%nodos%>", nodos)
+            salida = salida.replace("<%aristas%>", aristas)
+            print('---\n')
+            print( salida )
+            File().save('./gac/salida.graphml', salida)
         case _: # cancelar (por defecto)
             print('¡Hasta otro día!')
     
@@ -87,6 +106,7 @@ def main():
 def getTipoDeAtributos(ele: list[tuple[str, str|None]]):
     atributos = set()
     atributos.add('txt')
+    atributos.add('tag')
     # Mostramos según tengamos atributos o no
     try:
         if len(ele.atributos) > 0 :            
@@ -100,6 +120,45 @@ def getTipoDeAtributos(ele: list[tuple[str, str|None]]):
         atributos = atributos | getTipoDeAtributos(hijo) 
 
     return atributos
+
+
+def getNodosYAristas(ele: list[tuple[str, str|None]], clavesNominadas: list):
+    global numNodo
+    global numArista
+    nodos = []
+    aristas = []
+    datos = ''
+    
+    # Mostramos según tengamos atributos o no
+    try:
+        datos += f'\t<data key="{ clavesNominadas["tag"] }">{ele.nombre}</data>\n'
+        if len(ele.atributos) > 0 :            
+            for att in ele.atributos:
+                #print(clavesNominadas[ att[0] ], att[1])
+                datos += f'\t<data key="{clavesNominadas[ att[0] ]}">{att[1]}</data>\n'
+        else:
+            if len(ele.txt) > 0:
+                datos += f'\t<data key="{ clavesNominadas["txt"] }">{ele.txt}</data>\n'
+    except:
+        pass
+
+    # Creamos el nodo
+    nodo = f'<node id="n{numNodo}">\n'
+    numNodoPadre = numNodo
+    numNodo += 1
+    nodo += datos
+    nodo += '</node>'
+    nodos.append( nodo )
+
+    # recorremos los hijos
+    for hijo in ele.hijos:
+        aristas.append(f'<edge id="e{numArista}" source="n{numNodoPadre}" target="n{numNodo}"/>')
+        numArista += 1
+        nodHijo, ariHijo =  getNodosYAristas(hijo, clavesNominadas)
+        nodos.extend( nodHijo )
+        aristas.extend( ariHijo )
+
+    return nodos, aristas
 
 # Para visualizar el DOM analizado
 def printElemento(ele,profundidad):
