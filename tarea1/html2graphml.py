d5c7d9338d6d3d06e514280499bfef39175613bf
Luis Ferreira <luisferreira@iescastelar.com>
2025-12-12 13:23:07 +0100

    Genera Dot

--- tarea1/html2graphml.py
+++ tarea1/html2graphml.py
@@ -52,18 +52,32 @@ def main():
     # ---------------------------------------------------------------------
     grafo = ''
     #tipoDeGrafico = menuSalida()
-    tipoDeGrafico = 2
+    tipoDeGrafico = 1
     match tipoDeGrafico:
         case 1: # dot
-            grafo = GeneradorGrafoDot()
-            print(f'Grafo:\n{grafo.generate(dom)}')
-            pass
+            #grafo = GeneradorGrafoDot()
+            #print(f'Grafo:\n{grafo.generate(dom)}')
+            GRAPH_TYPE = 'dot'
+            TEMPLATE_MAIN = 'main.'+GRAPH_TYPE
+            TEMPLATE_KEYS = 'key.'+GRAPH_TYPE
+            TEMPLATE_NODES = 'node.'+GRAPH_TYPE
+            TEMPLATE_DATAS = 'data.'+GRAPH_TYPE
+            TEMPLATE_EDGES = 'edge.'+GRAPH_TYPE
+
+            claves = ''
+            listaDeNodos, listaDeAristas = getNodosYAristasDot(dom)
+            nodos = ''.join(listaDeNodos)
+            logging.debug(f'Nodos:\n{nodos}')
+            aristas = '\n'.join(listaDeAristas)
+            logging.debug(f'Aristas:\n{aristas}')
+
         case 2: # graphml
-            TEMPLATE_MAIN = 'graphml.xml'
-            TEMPLATE_KEYS = 'key.graphml'
-            TEMPLATE_NODES = 'node.graphml'
-            TEMPLATE_DATAS = 'data.graphml'
-            TEMPLATE_EDGES = 'edge.graphml'
+            GRAPH_TYPE = 'graphml'
+            TEMPLATE_MAIN = 'main.'+GRAPH_TYPE
+            TEMPLATE_KEYS = 'key.'+GRAPH_TYPE
+            TEMPLATE_NODES = 'node.'+GRAPH_TYPE
+            TEMPLATE_DATAS = 'data.'+GRAPH_TYPE
+            TEMPLATE_EDGES = 'edge.'+GRAPH_TYPE
 
             claves = getTipoDeAtributos(dom)
             clavesNominadas = {}
@@ -76,22 +90,25 @@ def main():
             for nombre, id in clavesNominadas.items():
                 claves += File().load(TEMPLATES_PATH+TEMPLATE_KEYS).replace("<%id%>", id).replace("<%nombre%>", nombre).replace("<%tipo%>", tipo) + '\n'
             logging.debug(f'Claves:\n{claves}')
-
+            
             listaDeNodos, listaDeAristas = getNodosYAristas(dom, clavesNominadas)
             nodos = ''.join(listaDeNodos)
             logging.debug(f'Nodos:\n{nodos}')
             aristas = '\n'.join(listaDeAristas)
-            logging.debug(f'Aristas:\n{aristas}')
-            
-            salida = File().load(TEMPLATES_PATH + TEMPLATE_MAIN)
-            salida = salida.replace("<%claves%>", claves)
-            salida = salida.replace("<%nodos%>", nodos)
-            salida = salida.replace("<%aristas%>", aristas)
-            logging.debug( salida )
-            File().save(TAREA_PATH + 'salida.graphml', salida)
+            logging.debug(f'Aristas:\n{aristas}')            
         case _: # cancelar (por defecto)
             print('¡Hasta otro día!')
-    
+
+
+
+    # Generamos la salida
+    salida = File().load(TEMPLATES_PATH + TEMPLATE_MAIN)
+    salida = salida.replace("<%claves%>", claves)
+    salida = salida.replace("<%nodos%>", nodos)
+    salida = salida.replace("<%aristas%>", aristas)
+    logging.debug( salida )
+    File().save(TAREA_PATH + 'salida.'+GRAPH_TYPE, salida)
+
     print('Hemos finalizado el procesado. Su archivo fue correctamente generado.')
     quit(1)
 
@@ -115,6 +132,62 @@ def getTipoDeAtributos(ele: list[tuple[str, str|None]]):
 
     return atributos
 
+
+
+def getNodosYAristasDot(ele: list[tuple[str, str|None]]):
+    global numNodo
+    global numArista
+    nodos = []
+    aristas = []
+    datos = ''
+    
+    # Mostramos según tengamos atributos o no
+    try:
+        plantilla = File().load(TEMPLATES_PATH+TEMPLATE_DATAS)
+        temp = plantilla.replace("<%key_id%>", 'label')  # o con key 'label'
+        temp = temp.replace("<%value%>", ele.nombre)
+        datos += temp + '\n'
+
+        if len(ele.atributos) > 0 :            
+            for att in ele.atributos:
+                temp = plantilla.replace("<%key_id%>", att[0] )
+                temp = temp.replace("<%value%>", att[1])
+                datos += temp + '\n'
+                # Aquí deberíamos pensar si queremos sacar los atributos de "style" como keys o dejarlos como un bloque.
+                # Pensemos que, con carácter general, los estilos irán en una hoja CSS externa por separación de responsabilidades.
+        else:
+            if len(ele.txt) > 0:
+                temp = plantilla.replace("<%key_id%>", "txt" )
+                temp = temp.replace("<%value%>", ele.txt)
+                datos += temp + '\n'
+    except:
+        pass
+
+    # Creamos el nodo
+    plantilla = File().load(TEMPLATES_PATH+TEMPLATE_NODES)
+    temp = plantilla.replace("<%id%>", f'n{numNodo}')
+    temp = temp.replace("<%datos%>", datos[:-1])
+    temp += '\n'
+    nodos.append( temp )
+    
+    # Actualizamos índice
+    numNodoPadre = numNodo
+    numNodo += 1
+    
+    plantillaArista = File().load(TEMPLATES_PATH+TEMPLATE_EDGES)
+    # recorremos los hijos
+    for hijo in ele.hijos:
+        #aristas.append( separador(' ')*2 + f'<edge id="e{numArista}" source="n{numNodoPadre}" target="n{numNodo}"/>')
+        temp = plantillaArista.replace("<%origen_id%>", f'n{numNodoPadre}')
+        temp = temp.replace("<%destino_id%>", f'n{numNodo}')
+        temp += '\n'
+        aristas.append( temp )
+        numArista += 1
+        nodHijo, ariHijo =  getNodosYAristasDot(hijo)
+        nodos.extend( nodHijo )
+        aristas.extend( ariHijo )
+
+    return nodos, aristas
 
 def getNodosYAristas(ele: list[tuple[str, str|None]], clavesNominadas: list):
     global numNodo
