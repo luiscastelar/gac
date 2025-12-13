9100685f92f43cb4248b95ab9a9abc6cbae47f85
Luis Ferreira <luisferreira@iescastelar.com>
2025-12-13 13:40:29 +0100

    Comentarios de trabajo seguro con archivos

--- tarea1/contentOfFile.py
+++ tarea1/contentOfFile.py
@@ -3,27 +3,36 @@ import os
 __all__ = ['File']
 
 class File():
+    # Gestión de archivos
 
     def load(self, filepath):
+        # Retorna todo el contenido del archivo o da error y finaliza programa
+
         if len(filepath)==0:
+            # Si no se le pasa nombre lo pide
             filepath = input('Introduce el nombre del archivo (ruta completa): ')
 
         try: 
             with open(filepath, 'r', encoding='utf-8') as f:
                 return f.read()
         except IOError as e:
-            print(f'Read failed: {e}')
-        #return open(nameOfFile).read()
+            print(f'Fallo de lectura: {e}')
+            quit(2)
 
     
-    def save(self, filepath, content):        
+    def save(self, filepath, content):
+        # Vuelca contenido en archivo o da error y finaliza programa.
+        # Sobreescribe el contenido anterior
+
         if len(filepath)==0:
             filepath = input('Introduce el nombre del archivo (ruta completa): ')
 
+        # Crea el directorio si no existe
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
         try:
             with open(filepath, 'w', encoding='utf-8') as f:
                 f.write(content)
         except IOError as e:
             print(f'Write failed: {e}')
+            quit(2)
         
