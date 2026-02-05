10548553c7d41d3b41dab38fe05063a2398b1e17
Luis Ferreira <luisferreira@iescastelar.com>
2026-02-05 10:38:40 +0100

    innecesario ya que se hizo a mano

--- tarea2/ejemplos/alumnos.sql
+++
@@ -1,9 +0,0 @@
--- Versión del servidor: 10.10.2-MariaDB-1:10.10.2+maria~ubu2204-log
-
-CREATE TABLE IF NOT EXISTS alumnos (
-  id int NOT NULL AUTO_INCREMENT,
-  nombre varchar(20),
-  apellidos varchar(50),
-  edad int,
-  PRIMARY KEY  (id)
-)
