774460b2f2bfbd09c5dbd92055e057a4fc7b2b9f
Luis Ferreira <luisferreira@iescastelar.com>
2026-02-07 11:14:41 +0100

    add seguridad sobre escrituras de sqlite

--- tarea2/templates/php/index.php
+++ tarea2/templates/php/index.php
@@ -13,11 +13,12 @@ if ($env === false) {
     die("No se pudo leer el fichero .env");
 }
 $db   = $env["DB"];
+$type = $env["TIPO_DB"]
 ?>
 <body class="bg-light">
     <div class="min-vh-100 d-flex align-items-center">
         <div class="container-md py-4">
-            <?php if (is_writable($db)) { ?>
+            <?php if (is_writable($db) || $type != 'sqlite') { ?>
             <h1 class="mb4">DB: %%TIPO_DB%% ➡️ UI: %%UI_TYPE%%</h1>
             <h2 class="mb4">Tablas disponibles en la base de datos</h2>
             <div class="list-group">
@@ -27,4 +28,3 @@ $db   = $env["DB"];
         </div>
     </div>
 </body>
-</html>
