a97c669f68529cbb24d6ec048746715a9be87d9e
Luis Ferreira <luisferreira@iescastelar.com>
2026-02-07 09:55:28 +0100

    check db is writable

--- tarea2/templates/php/index.php
+++ tarea2/templates/php/index.php
@@ -7,14 +7,23 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/default.css">
 </head>
+<?php
+$env = parse_ini_file(__DIR__ . "/.env");
+if ($env === false) {
+    die("No se pudo leer el fichero .env");
+}
+$db   = $env["DB"];
+?>
 <body class="bg-light">
     <div class="min-vh-100 d-flex align-items-center">
         <div class="container-md py-4">
+            <?php if (is_writable($db)) { ?>
             <h1 class="mb4">DB: %%TIPO_DB%% ➡️ UI: %%UI_TYPE%%</h1>
             <h2 class="mb4">Tablas disponibles en la base de datos</h2>
             <div class="list-group">
 %%BOTONES%%
             </div>
+            <?php } else { echo 'La base de datos "' . $db . '" es readonly.'; } ?>
         </div>
     </div>
 </body>
