0d2aa937f1d12ec898f6b0c777b63f43fac83429
Luis Ferreira <luisferreira@iescastelar.com>
2026-02-16 18:00:20 +0100

    refactor saco funciones utiles de db

--- tarea2/App.py
+++ tarea2/App.py
@@ -9,6 +9,7 @@ import settings
 from libs.baseDatos import BaseDatos
 from libs.tabla import Tabla
 from libs.columna import Columna
+import libs.dbComun as dbComun
 
 # ---------------------------------------------------------------------
 # Variables globales
@@ -35,26 +36,26 @@ def main():
     variablesDeEntorno = loadEnvironmentVar(tipoDB)
 
     # DONE: 4. Carga driver
-    db = getDriverDB(tipoDB)
+    db = dbComun.getDriverDB(tipoDB)
 
     # DONE: 5. Comandos "normalizados" vía plantilla
     db.comandosSQL = Env.get(settings.TAREA_PATH + 'templates/' + tipoDB + '/sql')
 
     # DONE: 6. Import en db pruebas
-    db.conn = getConexionDB(db, variablesDeEntorno)
+    db.conn = dbComun.getConexionDB(db, variablesDeEntorno)
 
     # DONE: 7. Importar datos
     db.name = variablesDeEntorno['NAME_DB']
     db.loadFromSQL(db.conn, db.name, sql)
 
     # DONE: 8. Generación de metadatos
-    metadatos = generacionDeMetadatos(db)
+    metadatos = dbComun.generacionDeMetadatos(db)
 
     # DONE: 9. Eleccion de salida
     # - vacio o 0 para preguntar por consola
     # - 1 para salida python
     # - 2 para salida php
-    tipoSalida, indexFile = TUI.eleccionDeSalida(1)
+    tipoSalida, indexFile = TUI.eleccionDeSalida(2)
     variablesDeEntorno.update({
         'tipoSalida': tipoSalida,
         'indexFile': indexFile
@@ -94,6 +95,7 @@ def initGlobalSettings():
     logging = settings.logging
     TUI.settings = settings
     utils.settings = settings
+    dbComun.settings = settings
     logging.debug('Variables globales cargadas en settings')
     return logging
 
@@ -147,76 +149,6 @@ def loadEnvironmentVar(tipoDB: str)->dict:
     else:
         utils.printError('Error datos de conexion', settings.EXIT['NOT_FOUND'])
     return variablesDeEntorno
-
-
-def getDriverDB(tipoDB):
-    # DONE: 4. Importacion de driver db según tipo (DAO)
-    match tipoDB:
-        case 'mariadb':
-            from libs import dbMariadb as db
-        case 'sqlite':
-            from libs import dbSqlite as db
-        case 'oracle-xe':
-            from libs import oracleDB as db
-        case _:
-            utils.printError(f'Gestor de BBDD {tipoDB} no disponible', settings.EXIT['FORMAT_ERROR'])
-
-    db.settings = settings  # Cargamos las variables globales en el driver que corresponda
-    return db
-
-
-def getConexionDB(db, variablesDeEntorno):
-    # DONE: Carga de metadatos
-    host = variablesDeEntorno['SERVER_DB']
-    port = variablesDeEntorno['PORT_DB']
-    user = variablesDeEntorno['USER_DB']
-    dbName = variablesDeEntorno['NAME_DB']
-    password = variablesDeEntorno['PASS_DB']
-    conn = db.getConn(host, port, user, password)
-    
-    ope = TUI.operacionesDeImportacion()
-    if ope <= 0:
-        utils.printError(f'Operación {ope} sobre db no disponible', 1)
-    if ope == 1:
-        db.dropDB(conn, dbName)
-    if ope <= 2:
-        conn = db.createDB(conn, dbName)
-    if ope <= 3:
-        pass
-    return conn
-
-
-def generacionDeMetadatos(db):
-    dbName = db.name
-    print(f'Generando metadatos de la base de datos {dbName}...')
-    comandosSQL = db.comandosSQL
-    
-    # DONE: Captura de estructura según tipo 'templates/{{dbName}}/sql'
-    metadatos = BaseDatos(dbName)
-    # Captura de tablas
-    sql = comandosSQL['show_tables'].replace("%%DB%%", dbName)
-    tablas = []
-    # Lista de columnas
-    atributos = ['TABLE_NAME', 'COLUMN_NAME', 'ORDINAL_POSITION', 'COLUMN_DEFAULT', 'IS_NULLABLE', 'DATA_TYPE', 'CHARACTER_MAXIMUM_LENGTH', 'NUMERIC_PRECISION', 'COLUMN_TYPE', 'COLUMN_KEY', 'COLUMN_COMMENT']
-    for tab in db.readSimpleList(db.conn, sql):
-        nombreTabla = tab[0]
-        # Captura de columnas
-        sql = comandosSQL['show_columns'].replace('%%TABLA%%', nombreTabla)
-        columnas = db.readSimpleList(db.conn, sql)
-        tabla = Tabla(nombreTabla)
-        for col in columnas:
-            columnaNombre = col[1]
-            columna = Columna(columnaNombre)
-            for i in range(0, len(col)):
-                k = atributos[i]
-                v = col[i]
-                setattr(columna, k, v)
-            tabla.columnas.append(columna)    # añadimos columna a columnas[]
-        tablas.append(tabla)                  # añadimos tabla a tablas[]
-    metadatos.tablas = tablas                   # añadimos tablas a db
-    logging.debug('Metadatos capturados:\n' + metadatos.str())
-    # print(metadatos.str())
-    return metadatos
 
 
 def getDriverSalida(tipoSalida):
---
+++ tarea2/libs/dbComun.py
@@ -0,0 +1,78 @@
+from . import utils
+from . import TUI
+from .baseDatos import BaseDatos
+#from . import baseDatos as BaseDatos
+from .tabla import Tabla
+from .columna import Columna
+
+settings = None
+
+
+def getDriverDB(tipoDB):
+    # DONE: 4. Importacion de driver db según tipo (DAO)
+    match tipoDB:
+        case 'mariadb':
+            from libs import dbMariadb as db
+        case 'sqlite':
+            from libs import dbSqlite as db
+        case 'oracle-xe':
+            from libs import oracleDB as db
+        case _:
+            utils.printError(f'Gestor de BBDD {tipoDB} no disponible', settings.EXIT['FORMAT_ERROR'])
+
+    db.settings = settings  # Cargamos las variables globales en el driver que corresponda
+    return db
+
+
+def getConexionDB(db, variablesDeEntorno):
+    # DONE: Carga de metadatos
+    host = variablesDeEntorno['SERVER_DB']
+    port = variablesDeEntorno['PORT_DB']
+    user = variablesDeEntorno['USER_DB']
+    dbName = variablesDeEntorno['NAME_DB']
+    password = variablesDeEntorno['PASS_DB']
+    conn = db.getConn(host, port, user, password)
+    
+    ope = TUI.operacionesDeImportacion()
+    if ope <= 0:
+        utils.printError(f'Operación {ope} sobre db no disponible', 1)
+    if ope == 1:
+        db.dropDB(conn, dbName)
+    if ope <= 2:
+        conn = db.createDB(conn, dbName)
+    if ope <= 3:
+        pass
+    return conn
+
+
+def generacionDeMetadatos(db):
+    dbName = db.name
+    print(f'Generando metadatos de la base de datos {dbName}...')
+    comandosSQL = db.comandosSQL
+    
+    # DONE: Captura de estructura según tipo 'templates/{{dbName}}/sql'
+    metadatos = BaseDatos(dbName)
+    # Captura de tablas
+    sql = comandosSQL['show_tables'].replace("%%DB%%", dbName)
+    tablas = []
+    # Lista de columnas
+    atributos = ['TABLE_NAME', 'COLUMN_NAME', 'ORDINAL_POSITION', 'COLUMN_DEFAULT', 'IS_NULLABLE', 'DATA_TYPE', 'CHARACTER_MAXIMUM_LENGTH', 'NUMERIC_PRECISION', 'COLUMN_TYPE', 'COLUMN_KEY', 'COLUMN_COMMENT']
+    for tab in db.readSimpleList(db.conn, sql):
+        nombreTabla = tab[0]
+        # Captura de columnas
+        sql = comandosSQL['show_columns'].replace('%%TABLA%%', nombreTabla)
+        columnas = db.readSimpleList(db.conn, sql)
+        tabla = Tabla(nombreTabla)
+        for col in columnas:
+            columnaNombre = col[1]
+            columna = Columna(columnaNombre)
+            for i in range(0, len(col)):
+                k = atributos[i]
+                v = col[i]
+                setattr(columna, k, v)
+            tabla.columnas.append(columna)    # añadimos columna a columnas[]
+        tablas.append(tabla)                  # añadimos tabla a tablas[]
+    metadatos.tablas = tablas                   # añadimos tablas a db
+    settings.logging.debug('Metadatos capturados:\n' + metadatos.str())
+    # print(metadatos.str())
+    return metadatos
